6c53f946f919c44b913f344e0ab1efc1c558f77c
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-09-11 09:53:24 +0300

    Added constructor for FileJackson

--- Budget Manager/task/src/budget/repository/FileJackson.java
+++ Budget Manager/task/src/budget/repository/FileJackson.java
@@ -1,6 +1,17 @@
 package budget.repository;
 
+import budget.domain.Account;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class FileJackson implements FileStorage {
+    private final Account account;
+    private final ObjectMapper objectMapper;
+
+    public FileJackson(Account account, ObjectMapper objectMapper) {
+        this.account = account;
+        this.objectMapper = objectMapper;
+    }
+
     @Override
     public boolean save() {
         return false;
