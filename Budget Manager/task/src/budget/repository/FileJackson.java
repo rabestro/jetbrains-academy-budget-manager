31e81ccd86d9d9f9f8292e43e7dc3687a250f66d
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-09-11 09:51:54 +0300

    Added empty class FileJackson

---
+++ Budget Manager/task/src/budget/repository/FileJackson.java
@@ -0,0 +1,13 @@
+package budget.repository;
+
+public class FileJackson implements FileStorage {
+    @Override
+    public boolean save() {
+        return false;
+    }
+
+    @Override
+    public boolean load() {
+        return false;
+    }
+}
