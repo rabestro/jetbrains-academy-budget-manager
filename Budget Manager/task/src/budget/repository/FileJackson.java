a3fa4623212a4b64f2172cbf31690a2f060c5789
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-09-11 09:55:03 +0300

    Added implementation for method save

--- Budget Manager/task/src/budget/repository/FileJackson.java
+++ Budget Manager/task/src/budget/repository/FileJackson.java
@@ -3,6 +3,8 @@ package budget.repository;
 import budget.domain.Account;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.IOException;
+
 public class FileJackson implements FileStorage {
     private final Account account;
     private final ObjectMapper objectMapper;
@@ -14,6 +16,15 @@ public class FileJackson implements FileStorage {
 
     @Override
     public boolean save() {
+        try {
+            objectMapper
+                    .writerWithDefaultPrettyPrinter()
+                    .writeValue(DATABASE, account);
+//            System.out.println("Purchases were saved!");
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return false;
     }
 
