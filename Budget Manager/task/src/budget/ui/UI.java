0c3ed15f16ce55ea3a91c046a91628b15e22c4b8
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-09-11 16:10:49 +0300

    Added class UI

---
+++ Budget Manager/task/src/budget/ui/UI.java
@@ -0,0 +1,10 @@
+package budget.ui;
+
+import java.math.BigDecimal;
+
+public class UI {
+
+    public void printTotal(final BigDecimal total) {
+        System.out.println("Total: $" + total);
+    }
+}
